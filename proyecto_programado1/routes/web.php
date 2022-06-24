b4b8664ce4d01e30cde6aedac4891f5f516f5226
LissetteCM <73190270+LissetteCM@users.noreply.github.com>
2022-06-24 09:50:15 -0600

    Update web.php

--- proyecto_programado1/routes/web.php
+++ proyecto_programado1/routes/web.php
@@ -12,9 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('plantillas.plantillaAdmin');
+});
+*/
+Route::get('/', function () {
+    return view('estudiantes.pagina');
 });
 
 
